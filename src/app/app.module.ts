56df337d9fd6ed2ad185daabb547f1796eb93feb
Hugo Oyola <hoyola@ucv.edu.pe>
2024-07-15 17:54:39 -0500

    feat: Add login component and dashboard layout
    The code changes include adding the login component and the dashboard layout. This commit introduces the necessary files and styles for these components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, AuthModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/dashboard/courses/courses.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CourseDialogComponent } from './components/course-dialog/course-dialog.component';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss',
+})
+export class CoursesComponent {
+  constructor(private matDialog: MatDialog) {}
+  openDialog(): void {
+    this.matDialog.open(CourseDialogComponent);
+  }
+}
